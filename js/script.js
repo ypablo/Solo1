5d23fb6d3d5171e365580b75f1137a83dfa94816
ypablo <yfedora@gmail.com>
2019-02-03 12:56:41 +0000

    Modify sript.js to fix sidebar minor bug

--- js/script.js
+++ js/script.js
@@ -25,4 +25,19 @@ function toggleNav() {
 	nav ? closeSlideMenu() : openSlideMenu();
 }
 
-   
+
+window.addEventListener("resize", function() {
+    if (window.matchMedia("(min-width: 768px)").matches) {
+        console.log("Screen width is at least 768px");
+		document.getElementById('side1').style.width = '10vw';
+		document.getElementById('container1').style.marginLeft = '0';
+		document.getElementById('top1').style.marginLeft = '0';
+		var element = document.getElementById("div");
+		element.classList.remove("open");
+		nav = false;
+    } else {
+        console.log("Screen less than 768px");
+		document.getElementById('side1').style.width = '0';
+		
+    }
+});  
